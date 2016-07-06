2f6c952ac14de0de958826f364e503c38a17357f
Denis Pavlyutchenko <Hixon10@yandex.ru>
2016-07-06 15:10:32 +0300

    Add angular gid

--- src/main/java/ru/pavlyutchenko/EmployeesDirectoryApplication.java
+++ src/main/java/ru/pavlyutchenko/EmployeesDirectoryApplication.java
@@ -4,30 +4,56 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
 import ru.pavlyutchenko.domain.Employee;
 import ru.pavlyutchenko.service.EmployeeRepository;
 
-import java.text.SimpleDateFormat;
 import java.time.LocalDateTime;
-import java.util.Arrays;
 
 @SpringBootApplication
 public class EmployeesDirectoryApplication {
 
     @Bean
     CommandLineRunner init(EmployeeRepository employeeRepository) {
-        return (evt) -> Arrays.asList(
-                "1, 1, 2, 3, 4, 3, 5".split(","))
-                .forEach(
-                        index -> {
-                            Employee employee = new Employee("firstName" + index,
-                                                            "lastName" + index,
-                                                            "middleName" + index,
-                                                            LocalDateTime.now(),
-                                                            "position" + index);
-                            employeeRepository.save(employee);
-                        });
+        return (evt) -> {
+            Employee employee = new Employee("Иван", "Петров", "Игорьевич", LocalDateTime.of(1982, 1, 3, 3, 5), "Сторож");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Иван", "Сирдюк", "Игорьевич", LocalDateTime.of(1982, 5, 9, 3, 5), "Сторож");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Иван", "Носов", "Анатольевич", LocalDateTime.of(1986, 5, 24, 3, 5), "Прораб");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Андрей", "Никитин", "Андреевич", LocalDateTime.of(1993, 6, 21, 3, 5), "Сантехник");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Сергей", "Веснушкин", "Сергеевич", LocalDateTime.of(1987, 2, 25, 3, 5), "Водитель");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Афанасий", "Киров", "Алексеевич", LocalDateTime.of(1981, 8, 13, 3, 5), "Писатель");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Елена", "Макоренко", "Денисович", LocalDateTime.of(1987, 12, 22, 3, 5), "Программист");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Иван2", "Сирдюк", "Игорьевич", LocalDateTime.of(1982, 5, 9, 3, 5), "Сторож");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Иван2", "Носов", "Анатольевич", LocalDateTime.of(1986, 5, 24, 3, 5), "Прораб");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Андрей2", "Никитин", "Андреевич", LocalDateTime.of(1993, 6, 21, 3, 5), "Сантехник");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Сергей2", "Веснушкин", "Сергеевич", LocalDateTime.of(1987, 2, 25, 3, 5), "Водитель");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Афанасий2", "Киров", "Алексеевич", LocalDateTime.of(1981, 8, 13, 3, 5), "Писатель");
+            employeeRepository.save(employee);
+
+            employee = new Employee("Елена2", "Макоренко", "Денисович", LocalDateTime.of(1987, 12, 22, 3, 5), "Программист");
+            employeeRepository.save(employee);
+        };
     }
 
 	public static void main(String[] args) {
--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,9 +1,6 @@
 var app = angular.module('app', ['ngAnimate', 'ngTouch', 'ui.grid']);
 
 app.controller('MainCtrl', ['$scope', '$http', 'uiGridConstants', function ($scope, $http, uiGridConstants) {
-    var today = new Date();
-    var nextWeek = new Date();
-    nextWeek.setDate(nextWeek.getDate() + 7);
 
     $scope.highlightFilteredHeader = function( row, rowRenderIndex, col, colRenderIndex ) {
         if( col.filters[0].term ){
@@ -19,66 +16,45 @@ app.controller('MainCtrl', ['$scope', '$http', 'uiGridConstants', function ($sco
             $scope.gridApi = gridApi;
         },
         columnDefs: [
-            // default
             {
                 field: 'firstName',
                 filter: {
-                    condition: uiGridConstants.filter.STARTS_WITH,
-                    placeholder: 'starts with'
-                }, headerCellClass: $scope.highlightFilteredHeader
-            },
-            // pre-populated search field
-            { field: 'gender', filter: {
-                term: '1',
-                type: uiGridConstants.filter.SELECT,
-                selectOptions: [ { value: '1', label: 'male' }, { value: '2', label: 'female' }, { value: '3', label: 'unknown'}, { value: '4', label: 'not stated' }, { value: '5', label: 'a really long value that extends things' } ]
-            },
-                cellFilter: 'mapGender', headerCellClass: $scope.highlightFilteredHeader },
-            // no filter input
-            { field: 'company', enableFiltering: false, filter: {
-                noTerm: true,
-                condition: function(searchTerm, cellValue) {
-                    return cellValue.match(/a/);
-                }
-            }},
-            // specifies one of the built-in conditions
-            // and a placeholder for the input
-            {
-                field: 'email',
-                filter: {
-                    condition: uiGridConstants.filter.ENDS_WITH,
-                    placeholder: 'ends with'
-                }, headerCellClass: $scope.highlightFilteredHeader
-            },
-            // custom condition function
-            {
-                field: 'phone',
-                filter: {
-                    condition: function(searchTerm, cellValue) {
-                        var strippedValue = (cellValue + '').replace(/[^\d]/g, '');
-                        return strippedValue.indexOf(searchTerm) >= 0;
-                    }
-                }, headerCellClass: $scope.highlightFilteredHeader
-            },
-            // multiple filters
-            { field: 'age', filters: [
-                {
-                    condition: uiGridConstants.filter.GREATER_THAN,
-                    placeholder: 'greater than'
+                    condition: uiGridConstants.filter.STARTS_WITH
                 },
-                {
-                    condition: uiGridConstants.filter.LESS_THAN,
-                    placeholder: 'less than'
-                }
-            ], headerCellClass: $scope.highlightFilteredHeader},
-            // date filter
-            { field: 'mixedDate', cellFilter: 'date', width: '15%', filter: {
-                condition: uiGridConstants.filter.LESS_THAN,
-                placeholder: 'less than',
-                term: nextWeek
-            }, headerCellClass: $scope.highlightFilteredHeader
+                headerCellClass: $scope.highlightFilteredHeader,
+                name: 'Имя'
             },
-            { field: 'mixedDate', displayName: "Long Date", cellFilter: 'date:"longDate"', filterCellFiltered:true, width: '15%',
+            {
+                field: 'lastName',
+                filter: {
+                    condition: uiGridConstants.filter.STARTS_WITH
+                },
+                headerCellClass: $scope.highlightFilteredHeader,
+                name: 'Фамилия'
+            },
+            {
+                field: 'middleName',
+                filter: {
+                    condition: uiGridConstants.filter.STARTS_WITH
+                },
+                headerCellClass: $scope.highlightFilteredHeader,
+                name: 'Отчество'
+            },
+            {
+                field: 'position',
+                filter: {
+                    condition: uiGridConstants.filter.STARTS_WITH
+                },
+                headerCellClass: $scope.highlightFilteredHeader,
+                name: 'Должность'
+            },
+            {
+                field: 'createdDate',
+                filter: {
+                    condition: uiGridConstants.filter.STARTS_WITH
+                },
+                headerCellClass: $scope.highlightFilteredHeader,
+                name: 'Дата рождения'
             }
         ]
     };
@@ -87,31 +63,12 @@ app.controller('MainCtrl', ['$scope', '$http', 'uiGridConstants', function ($sco
     $http.get('http://localhost:8080/employees')
         .success(function(data) {
             $scope.gridOptions.data = data;
-            $scope.gridOptions.data[0].age = -5;
 
-            data.forEach( function addDates( row, index ){
-                row.mixedDate = new Date();
-                row.mixedDate.setDate(today.getDate() + ( index % 14 ) );
-                row.gender = row.gender==='male' ? '1' : '2';
+            data.forEach( function convertDates( row, index ){
+                row.createdDate = row.createdDate[2] + "." + row.createdDate[1] + "." + row.createdDate[0];
+                // row.mixedDate = new Date();
+                // row.mixedDate.setDate(today.getDate() + ( index % 14 ) );
+                // row.gender = row.gender==='male' ? '1' : '2';
             });
         });
-
-    $scope.toggleFiltering = function(){
-        $scope.gridOptions.enableFiltering = !$scope.gridOptions.enableFiltering;
-        $scope.gridApi.core.notifyDataChange( uiGridConstants.dataChange.COLUMN );
-    };
-}])
-    .filter('mapGender', function() {
-        var genderHash = {
-            1: 'male',
-            2: 'female'
-        };
-
-        return function(input) {
-            if (!input){
-                return '';
-            } else {
-                return genderHash[input];
-            }
-        };
-    });
+}]);
